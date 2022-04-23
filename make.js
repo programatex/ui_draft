fa2a5ec7b6ffbaf024002dcf9375194233c933e9
haruki1234 <haruki070617@gmail.com>
2022-04-23 23:04:12 +0900

    make articles from the file

--- make.js
+++ make.js
@@ -18,33 +18,32 @@ class MAKE {
         let data = this.get();
         let d = data.replace(/\r/g,"").split("\n");
         let title = "";
-        let elm = document.createElement("div")
+        let elm = document.createElement("div");
         let index = [];
         let id = 0;
         elm.id = "page"
-        console.log(d);
         let p = 0;
         while (p<d.length) { // main
             let part = document.createElement("div");
             if (d[p].startsWith("# ")) {
                 if (title=="") {
                     title = d[p].slice(2);
-                    console.log(title)
+                    console.log(title);
                 }
                 part = document.createElement("h1");
                 part.innerHTML = d[p].slice(2);
-                part.className = "page"
+                part.className = "page";
                 part.id = "i"+id.toString();
-                index.push([1,d[p].slice(2),id])
+                index.push([1,d[p].slice(2),id]);
                 id++;
                 elm.appendChild(part);
             }
             else if (d[p].startsWith("## ")) {
                 part = document.createElement("h2");
                 part.innerHTML = d[p].slice(3);
-                part.className = "page"
+                part.className = "page";
                 part.id = "i"+id.toString();
-                index.push([2,d[p].slice(3),id])
+                index.push([2,d[p].slice(3),id]);
                 id++;
                 elm.appendChild(part);
             }
@@ -60,55 +59,55 @@ class MAKE {
                         break;
                     }
                     if (d[p].startsWith("```")) {
-                        let preelm = document.createElement("pre")
+                        let preelm = document.createElement("pre");
                         preelm.className = "page";
-                        let codeelm = document.createElement("code")
+                        let codeelm = document.createElement("code");
                         codeelm.className = "page";
                         p++;
                         while (true) {
                             if (p>=d.length||d[p].startsWith("```")) {
                                 break;
                             }
-                            codeelm.innerHTML += d[p]+"\n"
-                            p++
+                            codeelm.innerHTML += d[p]+"\n";
+                            p++;
                         }
                         if (pelm.innerHTML.length>0) {
                             part.appendChild(pelm);
                         }
 
-                        preelm.appendChild(codeelm)
-                        part.appendChild(preelm)
+                        preelm.appendChild(codeelm);
+                        part.appendChild(preelm);
 
                         pelm = document.createElement("p");
                         pelm.className = "page";
                     }
                     else {
-                        pelm.innerHTML += d[p].replace(/\ \ /g,"<br>")
+                        pelm.innerHTML += d[p].replace(/\ \ /g,"<br>");
                     }
                     p++;
                 }
                 if (part.children.length>0) {
-                    part.className = "page"
+                    part.className = "page";
                     elm.appendChild(part);
                 }
             }
             p++;
         }
-        console.log(index)
-        let ielm = document.createElement("ul")
-        ielm.className = "panellist"
-        ielm.id = "index"
+        console.log(index);
+        let ielm = document.createElement("ul");
+        ielm.className = "panellist";
+        ielm.id = "index";
         for (let i=0;i<index.length;i++) {
-            let lielm = document.createElement("li")
-            let buttonelm = document.createElement("button")
-            lielm.className = "panellist"
-            buttonelm.className = "panellist"
-            buttonelm.addEventListener("click",function() { jump("i"+index[i][2]) })
+            let lielm = document.createElement("li");
+            let buttonelm = document.createElement("button");
+            lielm.className = "panellist";
+            buttonelm.className = "panellist";
+            buttonelm.addEventListener("click",function() { jump("i"+index[i][2]) });
 
-            buttonelm.innerHTML = index[i][1]
+            buttonelm.innerHTML = index[i][1];
 
-            lielm.appendChild(buttonelm)
-            ielm.appendChild(lielm)
+            lielm.appendChild(buttonelm);
+            ielm.appendChild(lielm);
         }
         if (title!="") {
             title = title+" - ";
