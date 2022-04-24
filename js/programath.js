79dc92d291c81c28133137e566450eae7ed79130
Fumichannel <98387627+Fumichannel@users.noreply.github.com>
2022-04-24 21:10:40 +0900

    Update programath.js

--- js/programath.js
+++ js/programath.js
@@ -1,1 +1,9 @@
-
+function bintodec(strs) {
+    let result = 0;
+    for (let i = 0; i < strs.length; i++) {
+        if (strs[i]=="1") {
+            result += (2**(strs.length-i-1));
+        }
+    }
+    return result;
+}
